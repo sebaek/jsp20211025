00d83d80c4c2d93ef2d594d70c5f03c173f4d181
sebaek <sebaek@gmail.com>
2021-10-29 10:21:11 +0900

    param

---
+++ src/sample02javareview/MyRequest.java
@@ -0,0 +1,14 @@
+package sample02javareview;
+
+public class MyRequest {
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ src/sample02javareview/Sub.java
@@ -0,0 +1,8 @@
+package sample02javareview;
+
+public class Sub {
+	public void print(MyRequest request) {
+		
+		System.out.println(request.getName());
+	}
+}
