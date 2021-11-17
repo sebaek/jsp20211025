59a6c87c3fb7dac909d271ff7ce130465fd4c44d
sebaek <sebaek@gmail.com>
2021-11-17 16:07:09 +0900

    login filter

--- src/filter04/F10MemberFilter.java
+++ src/filter04/F10MemberFilter.java
@@ -1,13 +1,15 @@
 package filter04;
 
 import java.io.IOException;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -34,18 +36,21 @@ public class F10MemberFilter implements Filter {
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
 		// TODO Auto-generated method stub
 		// place your code here
-		HttpSession session = request.getSession();
+		HttpSession session = req.getSession();
 		
-		String logedIn = session.getAttribute("logedIn");
+		String logedIn = (String) session.getAttribute("logedIn");
 
 		// pass the request along the filter chain
 		if (logedIn != null && logedIn.equals("ok")) {
 			chain.doFilter(request, response);
 		} else {
-			String location = request.getContextPath() + "/filterex4/login";
-			response.sendRedirect(location);
+			String location = req.getContextPath() + "/filterex4/login";
+			res.sendRedirect(location);
 		}
 	}
 
