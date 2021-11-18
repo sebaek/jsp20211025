0a03a7e07d2eb1ff1ccf89d8e5167444cce3c941
sebaek <sebaek@gmail.com>
2021-11-18 10:29:33 +0900

    applistener5

---
+++ src/listener01/AppListener05.java
@@ -0,0 +1,43 @@
+package listener01;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Application Lifecycle Listener implementation class AppListener05
+ *
+ */
+// @WebListener
+public class AppListener05 implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public AppListener05() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+    	ServletContext application = sce.getServletContext();
+    	application.setAttribute("appAttr1", "appVal1");
+    }
+	
+}
+
+
+
+
+
+
+
