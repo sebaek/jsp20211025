29133281841d7b9e1f9946d84da9a930d1cd08e7
sebaek <sebaek@gmail.com>
2021-11-18 09:28:38 +0900

    servletContextlistener

---
+++ src/listener01/AppListener01.java
@@ -0,0 +1,45 @@
+package listener01;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class AppListener01
+ *
+ */
+@WebListener
+public class AppListener01 implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public AppListener01() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    	System.out.println("어플리케이션 실행됨. 리스너1 작동!!!!");
+    }
+	
+}
+
+
+
+
+
+
+
+
+
