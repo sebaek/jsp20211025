b1fc86f34111ffd5985c871dfda39dfd4814819e
sebaek <sebaek@gmail.com>
2021-11-19 09:33:04 +0900

    sql intro

---
+++ WebContent/WEB-INF/sql/01intro.sql
@@ -0,0 +1,22 @@
+-- Structured Query Language (SQL)
+
+-- 한 줄 주석
+
+-- 하나의 명령문은 세미콜론 끝남
+-- 하나의 명령문을 실행하는 방법
+-- 커서를 명령문안 (또는 맨끝)에 두고 ctrl+enter
+USE test;
+
+SELECT now();
+
+-- sql은 대소문자 구분을 안함 (단, 벤더나 버전에 따라 다를 수 있음)
+select NOW();
+
+-- sql은 작성 관습 : 키워드는 대문자로 작성
+SELECT NOW();
+
+SELECT now();
+
+
+
+
