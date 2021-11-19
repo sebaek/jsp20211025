5c8856db225e5562a6c5c6a21b0dc974a41d50d2
sebaek <sebaek@gmail.com>
2021-11-19 10:16:28 +0900

    select

---
+++ WebContent/WEB-INF/sql/02select01.sql
@@ -0,0 +1,35 @@
+-- A 테이블의 내용을 읽을 때
+-- SELECT 와 FROM 조합으로 작성
+-- SELECT : 조회할 열(columns) 명시
+-- FROM : Table 명 작성
+
+SELECT * FROM Customers; -- Customers 테이블 조회
+
+-- Employees 테이블 조회
+SELECT * FROM Employees;
+
+-- 조회할 열 선택
+SELECT LastName, FirstName FROM Employees;
+
+-- Customers 테이블의 CustomerName, Country 컬럼만 조회
+SELECT CustomerName, Country FROM Customers;
+
+SELECT customername, country FROM Customers;
+
+-- 모든 컬럼 조회 : *
+SELECT * FROM Customers;
+
+
+-- 테이블 컬럼(and 제약사항) 조회 (DESCRIBE, DESC)
+DESCRIBE Customers;
+DESC Customers;
+
+DESC Employees;
+
+-- Categories 테이블의 컬럼을 조회
+
+
+
+
+
+
