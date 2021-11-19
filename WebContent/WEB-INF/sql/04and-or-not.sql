e9bfcfb6767a5e2d33fa2544ad0231ba76362e4b
sebaek <sebaek@gmail.com>
2021-11-19 11:17:31 +0900

    and or not

--- WebContent/WEB-INF/sql/04and-or-not.sql
+++ WebContent/WEB-INF/sql/04and-or-not.sql
@@ -6,3 +6,25 @@ SELECT * FROM Customers WHERE Country = 'Mexico';
 SELECT * FROM Customers WHERE CustomerID < 10 AND Country = 'Mexico';
 
 -- Customers 테이블에서 Country 가 Germany 이고 City가 Berlin 인 행 조회
+SELECT * FROM Customers WHERE Country = 'Germany' AND City = 'Berlin';
+
+-- Customers 테이블에서 Country 가 Germany 이고 City가 Berlin 인 행의 CustomerName 조회
+SELECT CustomerName FROM Customers WHERE Country = 'Germany' AND City = 'Berlin';
+
+-- OR : 두 조건 중 하나만 만족
+SELECT * FROM Customers WHERE City = 'Paris' OR Country = 'UK';
+
+
+-- Customers 테이블에서 Country 가 Gernamy 이거나 Country 가 Spain 인 행 조회
+
+
+
+
+
+
+
+
+
+
+
+
