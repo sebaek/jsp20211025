6aa15726e0fdf5f04e63d8b2c2108253a6f016f6
sebaek <sebaek@gmail.com>
2021-11-19 09:36:44 +0900

    Update 01intro.sql

--- WebContent/WEB-INF/sql/01intro.sql
+++ WebContent/WEB-INF/sql/01intro.sql
@@ -17,6 +17,23 @@ SELECT NOW();
 
 SELECT now();
 
+SELECT * FROM Categories;
+SELECT * FROM Customers;
+SELECT * FROM Employees;
+SELECT * FROM OrderDetails;
+SELECT * FROM Orders;
+SELECT * FROM Products;
+SELECT * FROM Shippers;
+SELECT * FROM Suppliers;
+
+
+
+
+
+
+
+
+
 
 
 
