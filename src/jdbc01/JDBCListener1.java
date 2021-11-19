68f8ae23e4bf1eb3c880b90cbf03613680e03c57
sebaek <sebaek@gmail.com>
2021-11-19 15:19:26 +0900

    jdbc listener

--- src/jdbc01/JDBCListener1.java
+++ src/jdbc01/JDBCListener1.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
@@ -21,95 +22,85 @@ import org.mariadb.jdbc.MariaDbPoolDataSource;
 @WebListener
 public class JDBCListener1 implements ServletContextListener {
 
-    /**
-     * Default constructor. 
-     */
-    public JDBCListener1() {
-        // TODO Auto-generated constructor stub
-    }
+	private MariaDbPoolDataSource pool = null;
 
 	/**
-     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
-     */
-    public void contextDestroyed(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    }
+	 * Default constructor.
+	 */
+	public JDBCListener1() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-     * @see ServletContextListener#contextInitialized(ServletContextEvent)
-     */
-    public void contextInitialized(ServletContextEvent sce)  { 
-         // TODO Auto-generated method stub
-    	System.out.println("JDBC 리스너 시작!!!!!");
-    	
-    	ServletContext application = sce.getServletContext();
-    	
-    	String path = "/WEB-INF/db.properties";
-    	InputStream is = null;
-    	
-    	try {
-    		is = application.getResourceAsStream(path);
-    		
-    		Properties props = new Properties();
-    		props.load(is);
-    		
-    		System.out.println(props.get("user"));
-    		System.out.println(props.get("password"));
-    		System.out.println(props.get("ip"));
-    		System.out.println(props.get("schema"));
-    		
-    		// jdbc:mariadb://ip:port/schema?user=username&password=password
-    		
-    		String ip = props.getProperty("ip");
-    		String schema = props.getProperty("schema");
-    		String user = props.getProperty("user");
-    		String password = props.getProperty("password");
-    		
-    		String dburl = "jdbc:mariadb://" + ip + "/" + schema + "?user=" + user + "&password=" + password ;
-    		
-    		System.out.println(dburl);
-    		
-    		MariaDbPoolDataSource pool = new MariaDbPoolDataSource(dburl);
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent sce) {
+		if (pool != null) {
+			pool.close();
+		}
+	}
 
-    		Connection connection = pool.getConnection();
-    		Statement stmt = connection.createStatement();
-    		ResultSet rs = stmt.executeQuery("SELECT 333");
-    		if (rs.next()) {
-    			System.out.println(rs.getInt(1)); // 333 출력되면 쿼리 실행 OK
-    		}
-    		
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    	} finally {
-    		if (is != null) {
-    			try {
+	/**
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext application = sce.getServletContext();
+
+		String path = "/WEB-INF/db.properties";
+		InputStream is = null;
+
+		Connection connection = null;
+
+		try {
+			is = application.getResourceAsStream(path);
+
+			Properties props = new Properties();
+			props.load(is);
+
+			// jdbc:mariadb://ip:port/schema?user=username&password=password
+
+			String ip = props.getProperty("ip");
+			String schema = props.getProperty("schema");
+			String user = props.getProperty("user");
+			String password = props.getProperty("password");
+
+			String dburl = "jdbc:mariadb://" + ip + "/" + schema + "?user=" + user + "&password=" + password;
+
+			pool = new MariaDbPoolDataSource(dburl);
+
+			pool.setMaxPoolSize(1);
+
+			connection = pool.getConnection();
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT 333");
+			if (rs.next()) {
+				// 333 출력되면 쿼리 실행 OK
+				if (rs.getInt(1) == 333) {
+					System.out.println("데이터베이스 연결 완료!");
+				}
+			}
+
+			application.setAttribute("dbpool", pool);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if (is != null) {
+				try {
 					is.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-    		}
+			}
 		}
-    	
-    	
-    	
-    }
-	
+
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
