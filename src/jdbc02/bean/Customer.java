dfbf5ea7e1d2322afb5f0105b50ae3ac86e31b5c
sebaek <sebaek@gmail.com>
2021-11-22 11:52:06 +0900

    bean

---
+++ src/jdbc02/bean/Customer.java
@@ -0,0 +1,32 @@
+package jdbc02.bean;
+
+public class Customer {
+	private String customerName;
+	private String contactName;
+	private String address;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getContactName() {
+		return contactName;
+	}
+
+	public void setContactName(String contactName) {
+		this.contactName = contactName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
