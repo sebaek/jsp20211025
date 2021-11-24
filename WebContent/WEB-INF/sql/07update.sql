bbbcb9c777a1a4604a00b1c6073d61b48abbe86e
sebaek <sebaek@gmail.com>
2021-11-24 15:11:03 +0900

    update jdbc

---
+++ WebContent/WEB-INF/sql/07update.sql
@@ -0,0 +1,35 @@
+SELECT * FROM Customers ORDER BY CustomerID DESC;
+
+UPDATE Customers
+SET Address = 'yeoksam'
+WHERE CustomerID = 105;
+
+UPDATE Customers 
+SET 
+    Address = 'mancity',
+    City = 'Suwon',
+    PostalCode = '33333'
+WHERE
+    CustomerID = 105;
+    
+    
+-- JDBC 용
+UPDATE Customers
+SET 
+	CustomerName = ?,
+    ContactName = ?,
+    Address = ?,
+    City = ?,
+    PostalCode = ?,
+    Country = ?
+WHERE
+    CustomerID = ?
+
+
+
+
+
+
+
+
+
