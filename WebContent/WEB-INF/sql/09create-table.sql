9b353beef07269296bec64345238337dfeb75900
sebaek <sebaek@gmail.com>
2021-11-25 15:08:59 +0900

    create table

--- WebContent/WEB-INF/sql/09create-table.sql
+++ WebContent/WEB-INF/sql/09create-table.sql
@@ -36,8 +36,31 @@ SELECT * FROM mytable2;
 -- 연습: 2개의 테이블 mytable3, mytable4 만들기
 -- VARCHAR, CHAR 타입을 사용
 
+CREATE TABLE mytable3 (
+	name VARCHAR(11),
+    address VARCHAR(255),
+    postCode CHAR(5)
+);
 
+INSERT INTO mytable3 (name, address, postCode) VALUES ('kim', 'seoul', '00000');
+INSERT INTO mytable3 (name, address, postCode) VALUES ('lee', 'busan', '00');
 
+SELECT * FROM mytable3;
+
+SET sql_mode = 'PAD_CHAR_TO_FULL_LENGTH';
+SET sql_mode = '';
+
+CREATE TABLE mytable4 (
+	id VARCHAR(255),
+    nickName VARCHAR(255),
+    note VARCHAR(65535)
+);
+
+DESC mytable4;
+
+INSERT INTO mytable4 (id, nickName, note) VALUES ('donald', 'trump', 'president');
+
+select * from mytable4;
 
 
 
