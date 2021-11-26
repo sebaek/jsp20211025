c6b80b2473c31c4644f5aa0509b9095df4ef40d7
sebaek <sebaek@gmail.com>
2021-11-26 09:35:14 +0900

    java type and db type

--- src/jdbc08/JDBC31Servlet.java
+++ src/jdbc08/JDBC31Servlet.java
@@ -1,16 +1,21 @@
 package jdbc08;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 /**
  * Servlet implementation class JDBC31Servlet
  */
-@WebServlet("/JDBC31Servlet")
+@WebServlet("/jdbc08/s31")
 public class JDBC31Servlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -26,8 +31,29 @@ public class JDBC31Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// 0. 
+		ServletContext application = request.getServletContext();
+		DataSource ds = (DataSource) application.getAttribute("dbpool");
+		
+		
+		String sql = "INSERT INTO mytable10 (col1, col2, col3, col4, col5, col6, col7) VALUES (?, ?, ?, ?, ?, ?, ?)";
+		
+		try (Connection con = ds.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+			
+			pstmt.setInt(1, 9999999); // INT
+			pstmt.setDouble(2, 3.14); // DOUBLE
+			pstmt.setDouble(3, 1234567.1234); // DEC(10 , 3 )
+			pstmt.setString(4, "ABC"); // CHAR(3)
+			pstmt.setString(5, "Hello World"); // VARCHAR(255)
+			pstmt.setDate(6, java.sql.Date.valueOf("2021-11-26")); // DATE
+			pstmt.setTimestamp(7, java.sql.Timestamp.valueOf("2021-11-26 09:33:12"));
+			
+			pstmt.executeQuery();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -39,3 +65,11 @@ public class JDBC31Servlet extends HttpServlet {
 	}
 
 }
+
+
+
+
+
+
+
+
