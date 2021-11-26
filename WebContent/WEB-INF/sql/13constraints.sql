6bc9960a63d8e2ef5cb8e1ad8bd4b823d71d58cc
sebaek <sebaek@gmail.com>
2021-11-26 15:17:40 +0900

    not null unique

--- WebContent/WEB-INF/sql/13constraints.sql
+++ WebContent/WEB-INF/sql/13constraints.sql
@@ -11,3 +11,36 @@ SELECT * FROM mytable12;
 INSERT INTO mytable12 (col1, col2) VALUES ('abc', 'def');
 INSERT INTO mytable12 (col2) VALUES ('ghi');
 INSERT INTO mytable12 (col1) VALUES ('jkl');
+
+-- UNIQUE
+
+CREATE TABLE mytable13 (
+	col1 VARCHAR(5),
+    col2 VARCHAR(5) UNIQUE
+);
+
+SELECT * FROM mytable13;
+
+INSERT INTO mytable13 (col1, col2) VALUES ('abc', 'def');
+INSERT INTO mytable13 (col1, col2) VALUES ('abc', 'def');
+INSERT INTO mytable13 (col1, col2) VALUES ('abc', 'de');
+INSERT INTO mytable13 (col1) VALUES ('abc');
+INSERT INTO mytable13 (col1) VALUES ('abc');
+
+-- NOT NULL, UNIQUE
+CREATE TABLE mytable14 (
+	col1 VARCHAR(5),
+    col2 VARCHAR(5) NOT NULL UNIQUE
+);
+
+
+
+
+
+
+
+
+
+
+
+
