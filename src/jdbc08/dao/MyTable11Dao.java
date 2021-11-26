c4a84c4c7ff1d867572b908b3e5d7b5358368738
sebaek <sebaek@gmail.com>
2021-11-26 14:30:42 +0900

    insert , select, data type

--- src/jdbc08/dao/MyTable11Dao.java
+++ src/jdbc08/dao/MyTable11Dao.java
@@ -3,6 +3,10 @@ package jdbc08.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import jdbc08.bean.Bean11;
 
@@ -24,4 +28,40 @@ public class MyTable11Dao {
 		return false;
 	}
 
+	public List<Bean11> getList(Connection con) {
+		List<Bean11> list = new ArrayList<>();
+		String sql = "SELECT name, age, birth FROM mytable11";
+		
+		try (Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);) {
+			
+			while (rs.next()) {
+				Bean11 bean = new Bean11();
+				
+				bean.setName(rs.getString("name"));
+				bean.setAge(rs.getInt("age"));
+				bean.setBirth(rs.getDate("birth").toLocalDate());
+				
+				list.add(bean);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
