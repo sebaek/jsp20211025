b51336d65ffe57aeb3fed23fb2519babc79c8072
sebaek <sebaek@gmail.com>
2021-11-26 17:53:24 +0900

    Update 10create-table-datatype-date-time.sql

---
+++ WebContent/WEB-INF/sql/10create-table-datatype-date-time.sql
@@ -0,0 +1,22 @@
+USE test;
+
+CREATE TABLE mytable9 (
+	col1 DATE,
+    col2 DATETIME
+);
+
+DESC mytable9;
+
+INSERT INTO mytable9 (col1) VALUES ('2021-11-25');
+
+SELECT * FROM mytable9;
+
+INSERT INTO mytable9 (col2) VALUES ('2021-11-25 12:33:10');
+
+SELECT now();
+
+INSERT INTO mytable9 (col1, col2) VALUES (now(), now());
+
+
+
+
