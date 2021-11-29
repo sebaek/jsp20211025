1dab06c96e2afbb667ac6da7787349e76c7e3714
sebaek <sebaek@gmail.com>
2021-11-29 15:38:46 +0900

    join sample

--- WebContent/WEB-INF/sql/27join-sample.sql
+++ WebContent/WEB-INF/sql/27join-sample.sql
@@ -2,4 +2,5 @@ SELECT * FROM Categories;
 SELECT * FROM Products;
 
 SELECT p.ProductID, p.ProductName, c.CategoryName, p.Unit, p.Price
-;
+FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID
+ORDER BY 3, 2;
