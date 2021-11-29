32c226996258ecc966c195c94bc3205d810361cd
sebaek <sebaek@gmail.com>
2021-11-29 14:19:19 +0900

    left join

--- WebContent/WEB-INF/sql/23left-outer-join.sql
+++ WebContent/WEB-INF/sql/23left-outer-join.sql
@@ -5,3 +5,11 @@ SELECT * FROM mytable28Tel; -- 1, 3, 4
 
 SELECT * FROM mytable27Customer c LEFT OUTER JOIN mytable28Tel t ON c.id = t.customerId;
 SELECT * FROM mytable27Customer c LEFT JOIN mytable28Tel t ON c.id = t.customerId; -- outer 생략 가능
+
+-- 전화번호 없는 고객
+SELECT * 
+FROM mytable27Customer c LEFT JOIN mytable28Tel t ON c.id = t.customerId
+WHERE t.id IS NULL;
+
+-- 연습 : 주문한 적 없는 고객명 조회
+-- 사용테이블 : Customers, Orders
