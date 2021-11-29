e34b06cffb4077cc16b0d3dab1ddc786840860a9
sebaek <sebaek@gmail.com>
2021-11-29 15:21:46 +0900

    union

--- WebContent/WEB-INF/sql/26union.sql
+++ WebContent/WEB-INF/sql/26union.sql
@@ -17,4 +17,13 @@ SELECT firstName FROM mytable27Customer
 UNION ALL
 SELECT lastName FROM mytable27Customer;
 
+-- union 사용해서 full outer join
+SELECT * FROM mytable27Customer; -- 3, 4, 5, 6, 7
+SELECT * FROM mytable28Tel; -- 1, 3, 4
+
+SELECT * FROM mytable27Customer c LEFT JOIN mytable28Tel t ON c.id = t.customerId
+UNION
+SELECT * FROM mytable27Customer c RIGHT JOIN mytable28Tel t ON c.id = t.customerId;
+
+
 
