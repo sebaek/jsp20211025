c68dccea8cc3b3eec6b93feb6c246517203cf766
sebaek <sebaek@gmail.com>
2021-11-29 16:02:01 +0900

    product

--- WebContent/WEB-INF/sql/27join-sample.sql
+++ WebContent/WEB-INF/sql/27join-sample.sql
@@ -1,6 +1,10 @@
 SELECT * FROM Categories;
 SELECT * FROM Products;
 
-SELECT p.ProductID, p.ProductName, c.CategoryName, p.Unit, p.Price
-FROM Products p JOIN Categories c ON p.CategoryID = c.CategoryID
-ORDER BY 3, 2;
+SELECT 
+    c.CategoryName, p.ProductName, p.Unit, p.Price
+FROM
+    Products p
+        JOIN
+    Categories c ON p.CategoryID = c.CategoryID
+ORDER BY 1 , 2;
