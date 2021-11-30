c75c8b393ffab079be6ec26f933a3ea44820d22c
sebaek <sebaek@gmail.com>
2021-11-30 09:20:24 +0900

    sample

--- WebContent/WEB-INF/sql/28join.sql
+++ WebContent/WEB-INF/sql/28join.sql
@@ -6,3 +6,7 @@ SELECT c.CustomerName, o.OrderDate, o.EmployeeID, e.LastName, e.FirstName
 FROM Customers c JOIN Orders o ON c.CustomerID = o.CustomerID
 JOIN Employees e ON o.EmployeeID = e.EmployeeID
 ORDER BY 1;
+
+-- 어떤 고객이 어떤 배송자(Shipper)를 사용했는 지 조회
+-- 컬럼:고객명, 주문날짜, 배송자명, 배송자 전화번호
+-- 테이블:Customers, Orders, Shippers
