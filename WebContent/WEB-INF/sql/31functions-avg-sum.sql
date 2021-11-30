1b581756890d2ee96dc49ef33d4906150a40d68e
sebaek <sebaek@gmail.com>
2021-11-30 11:32:13 +0900

    Update 31functions-avg-sum.sql

--- WebContent/WEB-INF/sql/31functions-avg-sum.sql
+++ WebContent/WEB-INF/sql/31functions-avg-sum.sql
@@ -14,3 +14,10 @@ VALUES ('My Prod', 1, 1, '1KG'); -- null은 평균 계산에서 제외
 -- 연습1: SubQuery 사용해서 평균값 이하인 상품 목록
 
 -- 연습2: SubQuery 사용해서 평균값 이상인 상품 목록
+
+-- 연습3: 1996-07-08 에 주문한 상품의 총 수량 (Quantity)
+-- 사용테이블 : Orders, OrderDetails
+
+
+
+
