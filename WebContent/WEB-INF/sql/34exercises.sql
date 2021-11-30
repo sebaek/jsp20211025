cdaa144b4f0d89a0d26bf7468a0bc3fc4153b362
sebaek <sebaek@gmail.com>
2021-11-30 16:24:44 +0900

    exercises

--- WebContent/WEB-INF/sql/34exercises.sql
+++ WebContent/WEB-INF/sql/34exercises.sql
@@ -1,8 +1,14 @@
 -- 상품별 판매수량
-
+SELECT p.ProductID, p.ProductName, sum(od.Quantity) 
+FROM OrderDetails od JOIN Products p ON od.ProductID = p.ProductID
+GROUP BY p.ProductID
+;
 -- 상품별 매출액
-
+SELECT p.ProductID, p.ProductName, sum(od.Quantity) * p.Price '합'
+FROM OrderDetails od JOIN Products p ON od.ProductID = p.ProductID
+GROUP BY p.ProductID
+;
 -- 날짜별 매출액
 
-SELECT 3 * 3 ;
+
 
