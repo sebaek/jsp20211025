382c17f33ba6b7582282528228f2be19cd2c82e4
sebaek <sebaek@gmail.com>
2021-11-30 14:41:34 +0900

    having

--- WebContent/WEB-INF/sql/33having.sql
+++ WebContent/WEB-INF/sql/33having.sql
@@ -5,3 +5,8 @@ HAVING count(CustomerID) >= 10 -- people 가능
 
 -- 연습1 : 주문을 한 번만 한 사람들 조회 (CustomerName)
 -- table : Orders, Customers
+SELECT o.CustomerID, c.CustomerName, count(OrderID) 
+FROM Orders o JOIN Customers c ON o.CustomerID = c.CustomerID
+GROUP BY o.CustomerID
+HAVING count(OrderID) = 1
+;
