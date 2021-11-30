4a3bf19d6497085f0bba78b8f3faa4fbae5696a5
sebaek <sebaek@gmail.com>
2021-11-30 10:17:33 +0900

    functions count

--- WebContent/WEB-INF/sql/29functions.sql
+++ WebContent/WEB-INF/sql/29functions.sql
@@ -12,4 +12,13 @@ SELECT * FROM Customers ORDER BY Country;
 SELECT count(DISTINCT Country) FROM Customers;
 
 -- 연습1: Suppliers 의 city 컬럼의 값이 중복없이 몇 개 있는지?
+SELECT count(DISTINCT city) FROM Suppliers;
 -- 연습2: Suppliers 의 country 컬럼의 값이 중복없이 몇 개 있는지?
+SELECT count(DISTINCT country) FROM Suppliers;
+
+-- 연습3 : Seafood가 몇 번 주문되었는 지 조회
+
+
+
+
+
