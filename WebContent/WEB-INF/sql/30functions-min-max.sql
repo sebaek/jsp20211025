77bd34e8e08bae432ef2b0a1fe9d5c0b17767e34
sebaek <sebaek@gmail.com>
2021-11-30 11:13:51 +0900

    subquery

--- WebContent/WEB-INF/sql/30functions-min-max.sql
+++ WebContent/WEB-INF/sql/30functions-min-max.sql
@@ -9,3 +9,13 @@ SELECT MAX(Price) LargestPrice FROM Products; -- AS 생략 가능
 -- Employees 테이블에서 가장 이른 생일, 늦은 생일 조회
 SELECT MIN(BirthDate) FROM Employees;
 SELECT MAX(BirthDate) FROM Employees;
+
+-- 쿼리안에 쿼리 (nested query)
+SELECT * FROM Employees
+WHERE BirthDate = (SELECT MIN(BirthDate) FROM Employees); -- 주의: = 연산자 사용시 subquery 결과는 단일(1row)
+
+-- 연습1: 가장 나중에 태어난 사람(들) 조회
+
+-- 연습2: 가장 저렴한 가격의 상품 조회
+
+-- 연습3: 가장 비싼 가격의 상품 조회
