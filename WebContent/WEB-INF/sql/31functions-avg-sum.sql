0092d784c932e19a07df2c10700b8affbedfc249
sebaek <sebaek@gmail.com>
2021-11-30 11:30:07 +0900

    avg

---
+++ WebContent/WEB-INF/sql/31functions-avg-sum.sql
@@ -0,0 +1,16 @@
+-- avg
+SELECT AVG(Price) FROM Products; -- '28.866364' -> '28.496282'
+
+-- sum
+SELECT SUM(Price) FROM Products;
+
+DESC Products;
+SELECT * FROM Products;
+INSERT INTO Products (ProductName, SupplierID, CategoryID, Unit, Price)
+VALUES ('My Prod', 1, 1, '1KG', 0);
+INSERT INTO Products (ProductName, SupplierID, CategoryID, Unit)
+VALUES ('My Prod', 1, 1, '1KG'); -- null은 평균 계산에서 제외
+
+-- 연습1: SubQuery 사용해서 평균값 이하인 상품 목록
+
+-- 연습2: SubQuery 사용해서 평균값 이상인 상품 목록
