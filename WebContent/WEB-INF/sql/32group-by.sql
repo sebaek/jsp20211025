37487cae8194954f91271d02c90e1bbebca63d87
sebaek <sebaek@gmail.com>
2021-11-30 12:32:33 +0900

    max count subquery

--- WebContent/WEB-INF/sql/32group-by.sql
+++ WebContent/WEB-INF/sql/32group-by.sql
@@ -2,7 +2,7 @@ SELECT count(CustomerID) FROM Customers;
 
 SELECT Country, count(CustomerID) FROM Customers
 GROUP BY Country
-ORDER BY 1
+ORDER BY 2 DESC
 ;
 
 -- 연습1 : 나라별 Supplier 수 조회
@@ -12,5 +12,22 @@ ORDER BY 2
 ;
 
 -- 연습2 : 가장 많은 고객이 있는 나라(들) 조회
-
+SELECT 
+    *
+FROM
+    (SELECT 
+        Country, COUNT(CustomerID) Count
+    FROM
+        Customers
+    GROUP BY Country) B
+WHERE
+    count = (SELECT 
+            MAX(Count)
+        FROM
+            (SELECT 
+                Country, COUNT(CustomerID) Count
+            FROM
+                Customers
+            GROUP BY Country) A)
+;
 
