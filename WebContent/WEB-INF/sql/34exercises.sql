fa5f966623b152628043335f852b158418d03652
sebaek <sebaek@gmail.com>
2021-11-30 16:31:48 +0900

    exercises

--- WebContent/WEB-INF/sql/34exercises.sql
+++ WebContent/WEB-INF/sql/34exercises.sql
@@ -9,6 +9,20 @@ FROM OrderDetails od JOIN Products p ON od.ProductID = p.ProductID
 GROUP BY p.ProductID
 ;
 -- 날짜별 매출액
+SELECT o.OrderDate, od.ProductID,
+       p.ProductName, p.Price, od.Quantity,
+       sum(p.Price * od.Quantity) '합'
+FROM Orders o JOIN OrderDetails od ON o.OrderID = od.OrderID
+			  JOIN Products p ON od.ProductID = p.ProductID
+GROUP BY o.OrderDate
+ORDER BY o.OrderDate
+;
+
+
+
+
+
+
 
 
 
