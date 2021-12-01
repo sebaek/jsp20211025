5a7777fa628495c78d6ef6b31bc480f9ce1ebd80
sebaek <sebaek@gmail.com>
2021-12-01 11:10:56 +0900

    exercises

--- WebContent/WEB-INF/sql/36in.sql
+++ WebContent/WEB-INF/sql/36in.sql
@@ -10,3 +10,11 @@ WHERE Country IN ('Germany', 'Mexico', 'USA');
 
 SELECT * FROM Customers
 WHERE Country NOT IN ('Germany', 'Mexico', 'USA');
+
+
+-- 연습1 : 국가가 'USA', 'UK'인 Supplier들 조회 (IN 키워드 사용)
+
+-- 연습1 : 국가가 'USA', 'UK'가 아닌 Supplier들 조회 (NOT IN 키워드 사용)
+
+
+
