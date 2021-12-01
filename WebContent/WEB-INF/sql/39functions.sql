5e21e542d1c9bde99775108b7db60a77db7df5f6
sebaek <sebaek@gmail.com>
2021-12-01 16:23:33 +0900

    functions

--- WebContent/WEB-INF/sql/39functions.sql
+++ WebContent/WEB-INF/sql/39functions.sql
@@ -4,3 +4,29 @@ USE test;
 SELECT concat('abc', 'def') AS newStr; 
 
 SELECT EmployeeID, concat(LastName, ' ' ,FirstName) Name, BirthDate, Notes FROM Employees;
+
+-- SUBSTR : 부분문자열
+SELECT substr('hello wolrd', 1, 4); -- 원본문자열, 시작위치(1부터시작), 길이
+-- world 만 조회 되도록 
+SELECT substr('hello world', 7, 5);
+
+-- IFNULL : null을 다른 값으로
+SELECT CustomerName, IFNULL(ContactName, 'none') ContactName FROM Customers
+ORDER BY CustomerID DESC
+;
+
+SELECT * FROM Customers ORDER BY CustomerID DESC;
+
+-- coalesce : 첫번째로 null아닌 값
+SELECT coalesce(null, null, 'hello', null, 'world');
+
+SELECT CustomerName, coalesce(ContactName, 'none') FROM Customers ORDER BY CustomerID DESC;
+
+-- now
+SELECT now();
+
+-- 연습 : Customers 테이블 조회
+--      단, Address, City, PostalCode, Country 컬럼을
+--          ','를 구분자로 하여 연결된 값을 
+--          FullAddress라는 컬럼으로 조회
+--          또한 Address, City, PostalCode, Country 중 null값인 경우 빈 스트링'' 으로 연결 
