d5d1860994fba5e0e5b8d29d98115753b9977d87
sebaek <sebaek@gmail.com>
2021-12-01 14:27:50 +0900

    limit, page

--- WebContent/WEB-INF/sql/38limit.sql
+++ WebContent/WEB-INF/sql/38limit.sql
@@ -9,6 +9,34 @@ ORDER BY Price ASC
 LIMIT 3;
 
 -- 연습 1 : 가장 늦게 태어난 직원 3명 조회 
+SELECT * FROM Employees
+ORDER BY BirthDate DESC
+LIMIT 3
+;
+
+SELECT * FROM Customers
+ORDER BY CustomerID
+LIMIT 0, 5; 
+-- 첫번째수:시작 위치
+-- 두번째수:개수
+
+SELECT * FROM Customers
+ORDER BY CustomerID
+LIMIT 5, 5;
+
+SELECT * FROM Customers
+ORDER BY CustomerID
+LIMIT 10, 5;
+
+-- page 처리 활용
+-- 한 페이지에 10개의 데이터 조회
+-- 1 페이지 LIMIT 0, 10
+-- 2 페이지 LIMIT 10, 10
+-- 3 페이지 LIMIT 20, 10
+-- .....
+-- n 페이지 LIMIT (n-1)*10, 10
+
+
 
 
 
