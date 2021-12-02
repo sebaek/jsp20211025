1d05264f5adcf2a897e2c33de5b441c1499f30e4
sebaek <sebaek@gmail.com>
2021-12-02 11:10:46 +0900

    transaction

--- src/jdbc11/JDBC43Servlet.java
+++ src/jdbc11/JDBC43Servlet.java
@@ -1,11 +1,19 @@
 package jdbc11;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import jdbc11.bean.Bank;
+import jdbc11.dao.BankDao;
 
 /**
  * Servlet implementation class JDBC43Servlet
@@ -26,16 +34,79 @@ public class JDBC43Servlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String path = "/WEB-INF/view/jdbc11/v43.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		// 0. 
+		ServletContext application = request.getServletContext();
+		DataSource ds = (DataSource) application.getAttribute("dbpool");
+		BankDao dao = new BankDao();
+		
+		// 2.
+		int money = Integer.parseInt(request.getParameter("money"));
+		
+		// 3.
+		Connection con = null;
+		
+		try {
+			con = ds.getConnection();
+			
+			//   3.0 auto commit disabled (false)
+			con.setAutoCommit(false);
+			//   3.1 2번계좌 출금
+			Bank bank2 = dao.getBankById(con, 2);
+			bank2.setMoney(bank2.getMoney() - money);
+			dao.update(con, bank2);
+			
+			//   3.1 1번계좌 입금
+			Bank bank1 = dao.getBankById(con, 1);
+			bank1.setMoney(bank1.getMoney() + money);
+			dao.update(con, bank1);
+			
+			//   3.99 commit
+			con.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			//   3.exception rollback
+			if (con != null) {
+				try {
+					con.rollback();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		} finally {
+			if (con != null) {
+				try {
+					con.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// 4,5
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
